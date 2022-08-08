9fc613e53fd6bd31afbb92233886908ab38c7388
Cbannerman <cbannerman21@gmail.com>
2022-08-08 18:21:49 -0400

    added second passing test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,9 @@
+const { default: test } = require('node:test')
+const Manager = require('../lib/Manager')
+
+test('check for manager office', () => {
+    const manager = new Manager('DarthVader');
+    manager.officeNumber = '001';
+
+    expect(manager.officeNumber).toBe('001');
+});
